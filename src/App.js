0a33d63cb7cbb8d82addfd15c653822f2779e0e1
Sarath Sathyan S <sarathsathyans@outlook.com>
2023-10-21 15:14:06 +0530

    deployment 2 check

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Demo for Azure React App Deployment
-        </p>
+        <p>Demo for Azure React App Deployment</p>
+        <p>Deployment 2</p>
       </header>
     </div>
   );
